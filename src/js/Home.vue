08fe1d9a5b181300f57f74939613eb49d77f7b78
Your Name <you@example.com>
2020-11-25 15:56:51 -0400

    Post com dados

--- src/js/Home.vue
+++ src/js/Home.vue
@@ -1,5 +1,17 @@
 <template>
 	<div>
+		<div>
+			Título:<input type="text" v-model="addTitulo" />
+			<br/><br/>
+			Corpo: <textarea v-model="addCorpo"></textarea>
+			<br/><br/>
+
+			<button v-on:click="add">Adicionar</button>
+			
+		</div>
+
+		<hr/>
+		
 		<div v-for="post in posts" class="post_item">
 		<h5>#{{ post.id }} - {{ post.title }}</h5>
 		<p>{{ post.body }}</p>
@@ -13,16 +25,35 @@ import axios from "axios";
 export default {
 	data() {
 		return {
-			posts: []
+			posts: [],
+			addTitulo:'',
+			addCorpo:''
 		}
 	},
-  mounted() {
-	  axios
-	  	.get('https://jsonplaceholder.typicode.com/posts')
-	  	.then(json => {
-		  	this.posts = json.data
-	  	})
-  }
+	methods:{
+		add:function(){
+			axios
+				.post('https://jsonplaceholder.typicode.com/posts', {
+					userId:1,
+					title:this.addTitulo,
+					body:this.addCorpo
+				})
+				.then(json => {
+					console.log(json.data);
+				})
+				.finally(()=>{
+					this.addTitulo = '';
+					this.addCorpo = '';
+				});
+		}
+	},
+	mounted() {
+		axios
+			.get('https://jsonplaceholder.typicode.com/posts')
+			.then(json => {
+				this.posts = json.data
+			})
+	}
 }
 </script>
 
